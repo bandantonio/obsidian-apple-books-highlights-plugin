5d7ad89d0876f116768000ba5fe597c6a9785a31
Antonio <bandantonio@users.noreply.github.com>
2026-02-21 16:34:17 +0200

    test: update settings, plugindocs, and plugininfo

---
+++ test/modals/searchSuggestions.spec.ts
@@ -0,0 +1,14 @@
+import { describe, test } from 'vitest';
+// This functionality uses the Obsidian API which is hard to replicate in tests.
+// Will be revisited later.
+describe.todo('IBookHighlightsPluginSearchModal', () => {
+  test('Should filter books based on query');
+  test('Should display suggestions in the modal');
+  test('Should handle selection of a suggestion');
+  test('Should handle no results found');
+});
+
+describe.todo('OverwriteBookModal', () => {
+  test('Should show a modal window for bulk import');
+  test('Should show a modal window with book details');
+});
--- test/pluginInfo.spec.ts
+++ test/pluginInfo.spec.ts
@@ -1,5 +1,3 @@
-import os from 'os';
-import path from 'path';
 import { beforeEach, describe, expect, test } from 'vitest';
 
 declare module 'vitest' {
@@ -17,14 +15,6 @@ declare module 'vitest' {
 }
 
 import * as packageJson from '../package.json';
-import {
-  BOOKS_DB_PATH,
-  BOOKS_LIBRARY_COLUMNS,
-  BOOKS_LIBRARY_NAME,
-  HIGHLIGHTS_DB_PATH,
-  HIGHLIGHTS_LIBRARY_COLUMNS,
-  HIGHLIGHTS_LIBRARY_NAME,
-} from '../src/db/constants';
 
 describe('Plugin information', () => {
   beforeEach((context) => {
@@ -48,46 +38,5 @@ describe('Plugin information', () => {
   test('Check author information', ({ manifest }) => {
     expect(packageJson.author).toEqual(manifest.author);
     expect(manifest.authorUrl).toEqual('https://github.com/bandantonio');
-  });
-});
-
-describe('Plugin constants', () => {
-  test('Check database paths', () => {
-    expect(BOOKS_DB_PATH).toEqual(
-      path.join(os.homedir(), 'Library/Containers/com.apple.iBooksX/Data/Documents/BKLibrary/BKLibrary-1-091020131601.sqlite'),
-    );
-    expect(HIGHLIGHTS_DB_PATH).toEqual(
-      path.join(os.homedir(), 'Library/Containers/com.apple.iBooksX/Data/Documents/AEAnnotation/AEAnnotation_v10312011_1727_local.sqlite'),
-    );
-  });
-
-  test('Check database table names', () => {
-    expect(BOOKS_LIBRARY_NAME).toEqual('ZBKLIBRARYASSET');
-    expect(HIGHLIGHTS_LIBRARY_NAME).toEqual('ZAEANNOTATION');
-  });
-
-  test('Check database columns', () => {
-    expect(BOOKS_LIBRARY_COLUMNS).toEqual([
-      'ZASSETID',
-      'ZTITLE',
-      'ZAUTHOR',
-      'ZGENRE',
-      'ZLANGUAGE',
-      'ZLASTOPENDATE',
-      'ZDATEFINISHED',
-      'ZCOVERURL',
-    ]);
-
-    expect(HIGHLIGHTS_LIBRARY_COLUMNS).toEqual([
-      'ZANNOTATIONASSETID',
-      'ZFUTUREPROOFING5',
-      'ZANNOTATIONREPRESENTATIVETEXT',
-      'ZANNOTATIONSELECTEDTEXT',
-      'ZANNOTATIONLOCATION',
-      'ZANNOTATIONNOTE',
-      'ZANNOTATIONCREATIONDATE',
-      'ZANNOTATIONMODIFICATIONDATE',
-      'ZANNOTATIONSTYLE',
-    ]);
   });
 });
