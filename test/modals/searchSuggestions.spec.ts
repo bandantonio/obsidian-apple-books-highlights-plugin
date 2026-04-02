ab07bb302e3e259985f22e1e85fcbf09e2f10247
Antonio <bandantonio@users.noreply.github.com>
2026-04-02 20:07:19 +0300

    refactor: add test.todo for better clarity on pending tests

--- test/modals/searchSuggestions.spec.ts
+++ test/modals/searchSuggestions.spec.ts
@@ -2,13 +2,13 @@ import { describe, test } from 'vitest';
 // This functionality uses the Obsidian API which is hard to replicate in tests.
 // Will be revisited later.
 describe.todo('IBookHighlightsPluginSearchModal', () => {
-  test('Should filter books based on query');
-  test('Should display suggestions in the modal');
-  test('Should handle selection of a suggestion');
-  test('Should handle no results found');
+  test.todo('Should filter books based on query');
+  test.todo('Should display suggestions in the modal');
+  test.todo('Should handle selection of a suggestion');
+  test.todo('Should handle no results found');
 });
 
 describe.todo('OverwriteBookModal', () => {
-  test('Should show a modal window for bulk import');
-  test('Should show a modal window with book details');
+  test.todo('Should show a modal window for bulk import');
+  test.todo('Should show a modal window with book details');
 });
